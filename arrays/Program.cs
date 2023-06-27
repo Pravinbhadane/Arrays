dfebb8249c6caa8e003619f7987f6a8dec0bb8aa
Pravin Bhadane <pravinbhadane60@gmail.com>
2023-06-27 22:13:06 +0530

    Add project files.

---
+++ arrays/Program.cs
@@ -0,0 +1,37 @@
+﻿public class Program
+{
+    static void Main(string[] args)
+    {
+
+        int[] numbers = new int[10] { 78, 45, 12, 89, 23, 90, 11, 4, 8, 9 };
+        int[] nos = new int[10] { 1, 2, 3, 4, 5, 0, 0, 0, 0, 0 };
+
+        foreach (var item in numbers)
+        {
+            Console.WriteLine(item);
+        }
+
+
+        Array.Sort(numbers);
+        Console.WriteLine("------after sort------");
+        foreach (var item in numbers)
+        {
+            Console.WriteLine(item);
+        }
+        Array.Reverse(numbers);
+        Console.WriteLine("------after reverse------");
+        foreach (var item in numbers)
+        {
+            Console.WriteLine(item);
+        }
+
+        Array.Copy(numbers, 3, nos, 5, 3);
+        Console.WriteLine("------------------");
+        foreach (var item in nos)
+        {
+            Console.WriteLine(item);
+        }
+
+
+    }
+}
---
+++ arrays/String_Array.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace arrays
+{
+    public class String_Array
+    {
+        static void Main(string[] args)
+        {
+
+            string[] names = new string[10] { "pravin", "gaurav", "pawan", "mayur", "kartik", "piyush", "rishabh", "pritesh", "pranav", "pankaj" };
+
+            string[] names2 = new string[10];
+
+            foreach (var item in names)
+            {
+                Console.WriteLine(item);
+            }
+
+            Array.Sort(names);
+            Console.WriteLine("------after sort------");
+            foreach (var item in names)
+            {
+                Console.WriteLine(item);
+            }
+
+            Array.Reverse(names);
+
+            Console.WriteLine("------after reverse------");
+            foreach (var item in names)
+            {
+                Console.WriteLine(item);
+            }
+
+            Array.Copy(names, 1, names2, 0, 8);
+            Console.WriteLine("--after copy------");
+            foreach (var item in names2)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
